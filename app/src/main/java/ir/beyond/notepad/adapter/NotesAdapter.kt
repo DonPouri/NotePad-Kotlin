0de14f9f6aba5fe9a61836be404006b6b96e583d
RezaFarhoodi4407 <rezafarhoodi4407@gmail.com>
2025-01-02 21:02:07 +0330

    notesAdapter complited

--- app/src/main/java/ir/beyond/notepad/adapter/NotesAdapter.kt
+++ app/src/main/java/ir/beyond/notepad/adapter/NotesAdapter.kt
@@ -2,14 +2,36 @@ package ir.beyond.notepad.adapter
 
 import android.app.Activity
 import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import ir.beyond.notepad.databinding.ListItemNotesBinding
 
 class NotesAdapter(
     private val context: Activity ,
     private val allData : ArrayList<RecyclerNotesModel>
-    ) {
+    ):RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
 
-    inner class NotesViewHolder(private val view : ListItemNotesBinding){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        NotesViewHolder(
+            ListItemNotesBinding.inflate(context.layoutInflater , parent , false)
+        )
+
+    override fun getItemCount(): Int = allData.size
+
+    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
+        holder.setData(allData[position])
+    }
+
+    inner class NotesViewHolder(
+        private val binding : ListItemNotesBinding)
+        :RecyclerView.ViewHolder(binding.root){
+
+        fun setData(data : RecyclerNotesModel){
+            binding.txtTitleNotes.text = data.title
+
+        }
 
     }
 
