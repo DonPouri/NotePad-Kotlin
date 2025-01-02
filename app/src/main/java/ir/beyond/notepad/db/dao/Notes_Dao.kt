125d7c24c61620a9dd51a348ce4c564327b737b8
RezaFarhoodi4407 <rezafarhoodi4407@gmail.com>
2025-01-02 21:26:13 +0330

    add getNotesForRrecycler fun in dao

--- app/src/main/java/ir/beyond/notepad/db/dao/Notes_Dao.kt
+++ app/src/main/java/ir/beyond/notepad/db/dao/Notes_Dao.kt
@@ -1,12 +1,16 @@
 package ir.beyond.notepad.db.dao
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.util.Log
+import ir.beyond.notepad.adapter.RecyclerNotesModel
 import ir.beyond.notepad.db.DBHelper
 import ir.beyond.notepad.db.model.DBNotesModel
 
 class Notes_Dao(private val db:DBHelper
 ) {
 
+    private lateinit var cursor : Cursor
     private val contentValues = ContentValues()
 
     fun saveNotes(notes : DBNotesModel):Boolean{
@@ -26,5 +30,41 @@ class Notes_Dao(private val db:DBHelper
         contentValues.put(DBHelper.NOTES_DELETE_STATE ,notes.deletestate)
         contentValues.put(DBHelper.NOTES_DATE ,notes.date)
     }
+
+    fun getNotesForRecycler(value:String):ArrayList<RecyclerNotesModel>{
+
+        val database  = db.readableDatabase
+        val query = "SELECT ${DBHelper.NOTES_ID} , ${DBHelper.NOTES_TITLE} " +
+                " FROM ${DBHelper.NOTES_TABLE}" +
+                "WHERE ${DBHelper.NOTES_DELETE_STATE} = ?"
+        cursor = database.rawQuery(query, arrayOf(value))
+        val data = getDataForRecycler()
+        cursor.close()
+        database.close()
+        return data
+    }
+
+    private fun getDataForRecycler(): ArrayList<RecyclerNotesModel> {
+
+        val data = ArrayList<RecyclerNotesModel>()
+
+        try {
+
+            if (cursor.moveToFirst()){
+                do{
+                    val id = cursor.getInt(getIndext(DBHelper.NOTES_ID))
+                    val title = cursor.getString(getIndext(DBHelper.NOTES_TITLE))
+                    data.add(RecyclerNotesModel(id , title))
+                }while (cursor.moveToNext())
+            }
+
+        }catch (e: Exception) {
+            Log.e("ERROR" , e.message.toString())
+        }
+        return data
+
+    }
+
+    fun getIndext(name:String) = cursor.getColumnIndex(name)
 
 }
